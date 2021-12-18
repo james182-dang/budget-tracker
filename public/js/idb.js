bfe09f7c6948be9c53a8947208e96c5e3a7e5dd4
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-18 14:54:44 -0600

    set up service worker, idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,41 @@
+// create variable to hold db connection
+let db;
+
+// establish a connection to indexedDB database called 'budget-tracker' and set it to version 1
+const request = indexedDB.open('budget-tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database
+    const db = event.target.result;
+    
+    // create an object store (table) called `new-budget`, set it to have an auto incrementing primary key of sorts
+    db.createObjectStore('new-budget', { autoIncrement: true });
+}
+
+// upon successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadBudget() function to send all local db data to api
+    if (navigator.onLine) {
+        // uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a budget update and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new-budget'], 'readwrite');
+
+    // access the object store for `new-budget`
+    const budgetObjectStore = transaction.objectStore('new-budget');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+}
